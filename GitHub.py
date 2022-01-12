1eb8557f40166e18dcaaf4fe13c768efb336ac9d
Your Name <you@example.com>
2022-01-12 13:44:50 +0100

    Meriem Moroco

---
+++ GitHub.py
@@ -0,0 +1,2 @@
+print("this is my first github projet")
+print("")
---
+++ gitProject.py
@@ -0,0 +1,7 @@
+print("This is the first project going to GITHUB")
+Print("Thanks Prof Okba ☕")
+print("Gaining access is cool")
+print("Finally i am here ...")
+print("Never give up")
+
+
