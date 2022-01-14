3d1ecbe83eaddf5a9231441984c879e1d0aa56e2
Alaeddine TELLI <telliala@gmail.com>
2022-01-14 18:23:00 +0100

    new commit

--- gitProject.py
+++ gitProject.py
@@ -2,6 +2,6 @@ print("This is the first project going to GITHUB")
 print("Thanks Prof Okba ☕")
 print("Gaining access is cool")
 print("Finally i am here ...")
-print("Alaeddine ")
+print("Alaeddine new commit !")
 
 
