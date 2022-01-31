d2a6353f741010cbb64c25f2b22a515ff6da0c53
Mezaouli <mezaoulimoha@gmail.com>
2022-01-31 19:18:00 +0100

    Class category add + documentation

--- Category.py
+++ Category.py
@@ -1,12 +1,11 @@
-import datetime
-
-
+import datetime # To use time lib
 
 class Category:
     '''this class for Category'''
 
-    create_at = datetime.datetime.now()
-    update_at = None
-    
-    def __init__(self,name):
+    create_at = datetime.datetime.now() #To know when the category was created
+    update_at = None #To know if we did any update was done
+
+#The fontion __init__ to give a name to a category, it will be created automatically with the creation of any obj and it take name as attribute
+    def __init__(self,name): 
         self.name = name
