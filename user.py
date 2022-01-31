3defa8ecad7e57221b6878cc19c6153fd8b28d5b
Alaeddine TELLI <telliala@gmail.com>
2022-01-31 19:26:17 +0100

    fix user doc

--- user.py
+++ user.py
@@ -5,13 +5,15 @@ import datetime
 class User:
     '''this class for user'''
 
-    create_at = datetime.datetime.now()
-    update_at = None
-    status = False
-    user_type = "01"
+    create_at = datetime.datetime.now() #user create date 
+    update_at = None #user create date 
+    status = False #user status , True = active, False = deactive
+    user_type = "01"  # user type '00': Admin, '01': student user
+
     
     def __init__(self,name,email,password,phone,picture):
-        self.name = name
+        ''' define the attribute in __init__ function allow to create the object easly '''
+        self.name = name 
         self.email = email
         self.password = password
         self.phone = phone
