dbb5963f66a74a46e14b4b8b3956775d943dc453
Mezaouli <mezaoulimoha@gmail.com>
2022-01-31 19:02:29 +0100

    Class category add

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from unicodedata import name
 from user import User
-
+from Category import Category
 
 dhikra = User("dhikra","dhikra@gmail.com","1234hhghkiL","06596548","path")
 # moh = User("mohammed","moh@gmail.com","14589jilkh","0264935","pathpicture")
 #yassine = User("yssin","yassin@gmail.com","hfgk125","25634","picturepath",False)
+Science = Category("Science") #Test of category class
+
 
 print(dhikra.create_at)
 print(dhikra.status)
+print(Science.name)
