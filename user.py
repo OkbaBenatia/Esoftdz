cdc52d3a689ae011fab166abcc2ba3bd519c4598
Alaeddine TELLI <telliala@gmail.com>
2022-02-04 23:44:15 +0100

    fix pep8 standard

--- user.py
+++ user.py
@@ -1,21 +1,15 @@
 import datetime
-
-
-
 class User:
-    '''this class for user'''
-
-    create_at = datetime.datetime.now() #user create date 
-    update_at = None #user create date 
-    status = False #user status , True = active, False = deactive
+    # this class for user
+    create_at = datetime.datetime.now() # user create date 
+    update_at = None  # user create date 
+    status = False  # user status , True = active, False = deactive
     user_type = "01"  # user type '00': Admin, '01': student user
-
     
-    def __init__(self,name,email,password,phone,picture):
-        ''' define the attribute in __init__ function allow to create the object easly '''
-        self.name = name 
+    def __init__(self, name, email, password, phone, picture):
+        # define the attribute in __init__ function allow to create the object easly
+        self.name = name
         self.email = email
         self.password = password
         self.phone = phone
         self.picture = picture
-        
