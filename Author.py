8d2ee4d7f0a75f3101c3196fac8a4bfa2dd4c731
Belk-hachi <johndoe@exam>
2022-02-06 00:35:52 +0100

    fix pep8 standard

--- Author.py
+++ Author.py
@@ -1,13 +1,14 @@
 import datetime
 
-# 
-class Author:
-    '''class for the Author object'''
 
-    create_At = datetime.datetime.now()  # record the date and time of object creation
+class Author:
+    """ class for the Author object"""
+
+    create_At = datetime.datetime.now()  # Object creation date and time
     update_At = None
 
-    def __init__(self, name, phone, email , country , address , websiteurl , twitterurl , userid):
+    def __init__(self, name, phone, email, country, address, websiteurl,
+                 twitterurl, userid):
         self.name = name
         self.phone = phone
         self.email = email
@@ -15,5 +16,4 @@ class Author:
         self.address = address
         self.websiteurl = websiteurl
         self.twitterurl = twitterurl
-        self.userid = userid   # Log the user who created the author object
-
+        self.userid = userid  # Log the user who created the author object
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from unicodedata import name
 from user import User
 from Category import Category
+from Author import Author
 
 dhikra = User("dhikra","dhikra@gmail.com","1234hhghkiL","06596548","path")
 # moh = User("mohammed","moh@gmail.com","14589jilkh","0264935","pathpicture")
 #yassine = User("yssin","yassin@gmail.com","hfgk125","25634","picturepath",False)
 Science = Category("Science") #Test of category class
+author1 = Author('James M. McPherson', '0555 555 555','j.mcpherson@gmail.com','USA',
+        'His adress', 'https://www.goodreads.com/author/show/12144.James_M_McPherson','@jamesMc',1 )
 
 
 print(dhikra.create_at)
@@ -14,3 +17,5 @@ print(Science.name)
 
 # using built-in vars to show all attributes
 print (vars(dhikra))
+print (dhikra.__doc__)
+print(author1.__doc__)
