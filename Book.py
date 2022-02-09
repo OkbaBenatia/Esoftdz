4f7299d4e759553f230d00022896e2ced8b19fa0
Boubaker Benaissa <boubaker.benaissa@hotmail.fr>
2022-02-09 14:18:13 +0100

    Book

---
+++ Book.py
@@ -0,0 +1,18 @@
+import datetime
+
+class Book:
+    create_at = datetime.datetime.now()
+    update_at = None
+
+
+    def __init__(self, name, version, numpage, abstract, yearpublish, numviews, language, picture, path)
+        self.name = name
+        self.version = version
+        self.numpage = numpage
+        self.abstract = abstract
+        self.yearpublish = yearpublish
+        self.numviews = numviews
+        self.language = language
+        self.picture = picture
+        self.path = path
+
